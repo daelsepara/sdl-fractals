1244012f83bc49997a0de068c0ec69cb7cb09c7a
Dael <dael.separa@gmail.com>
2023-09-25 16:37:08 +0800

    Adjust parameters

--- src/Mandelbrot.cpp
+++ src/Mandelbrot.cpp
@@ -7,9 +7,9 @@
 
 int main(int argc, char **argv)
 {
-    auto scale_x = 512;
-    auto scale_y = 512;
-    auto escape_time_threshold = 255;
+    auto scale_x = 1024;
+    auto scale_y = 1024;
+    auto escape_time_threshold = 32;
     auto escape_value_threshold = 2.0;
 
     auto grid = std::vector<std::vector<short int>>(scale_y);
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 
     std::cout << "P3" << std::endl;
     std::cout << scale_x << " " << scale_y << std::endl;
-    std::cout << "255" << std::endl;
+    std::cout << escape_time_threshold << std::endl;
 
     auto min_x = -2.0;
     auto max_x = 1.0;
@@ -38,6 +38,7 @@ int main(int argc, char **argv)
         for (auto x = 0; x < scale_x; x++)
         {
             short int t = 0;
+
             auto z = std::complex(0.0, 0.0);
             auto cx = min_x + (double)x * dx;
             auto cy = min_y + (double)(scale_y - y - 1) * dy;
